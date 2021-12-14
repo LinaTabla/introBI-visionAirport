d8f8d39bb3fa2da75e237898ad4eb7640b37ff37
yfish1 <73119869+yfish1@users.noreply.github.com>
2021-12-14 17:40:59 +0100

    Add files via upload
    Klant dubbelcheck!
    (TODO: Luchthavens)

---
+++ scripts/Cleanse_Klant.sql
@@ -0,0 +1,13 @@
+use VisionAiport_OLTP;
+
+
+INSERT INTO [CLEANSED].klant
+	SELECT 
+	CAST(Vluchtid AS int),
+	CAST(Operatie AS decimal(18,2)),
+	CAST(Faciliteiten AS decimal(18,2)),
+	NULLIF(CAST(Shops AS decimal(18,2)), '')
+	FROM [RAW].[export_klant];
+
+
+select * from CLEANSED.klant;
