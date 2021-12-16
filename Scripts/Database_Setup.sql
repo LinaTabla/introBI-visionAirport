76a1ff24828e574113097e5abca23c84e4319c26
LinaTabla <lina.tabla@hotmail.com>
2021-12-16 14:31:03 +0100

    Order fix

--- Scripts/Database_Setup.sql
+++ Scripts/Database_Setup.sql
@@ -1,13 +1,12 @@
--- 1. create DWH database
 DROP DATABASE IF EXISTS VisionAirport_DWH;
 CREATE DATABASE VisionAirport_DWH;
 
--- 2. create OLTP database
 DROP DATABASE IF EXISTS VisionAirport_OLTP;
 CREATE DATABASE VisionAirport_OLTP;
 GO
+USE VisionAirport_OLTP;
+GO
 
--- 3. create schema's in db VisionAirport_OLTP
 CREATE SCHEMA [RAW];
 GO
 CREATE SCHEMA [ARCHIVE];
