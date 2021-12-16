7c627e36d47816a0310c8bad03232b69f18dd541
LinaTabla <lina.tabla@hotmail.com>
2021-12-16 14:09:25 +0100

    Added a batch separator before schema create

--- Scripts/Database_Setup.sql
+++ Scripts/Database_Setup.sql
@@ -5,7 +5,7 @@ CREATE DATABASE VisionAirport_DWH;
 -- 2. create OLTP database
 DROP DATABASE IF EXISTS VisionAirport_OLTP;
 CREATE DATABASE VisionAirport_OLTP;
-USE VisionAirport_OLTP;
+GO
 
 -- 3. create schema's in db VisionAirport_OLTP
 CREATE SCHEMA [RAW];
@@ -14,5 +14,3 @@ CREATE SCHEMA [ARCHIVE];
 GO
 CREATE SCHEMA [CLEANSED];
 GO
-
--- 4. import flat data files in [VisionAirport_OLTP].[RAW] > Import Wizard
