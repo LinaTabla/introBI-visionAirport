43592d73a8f655103b085e4a27762afe2229d9d8
LinaTabla <lina.tabla@hotmail.com>
2021-12-16 13:14:54 +0100

    Added create VisionAirport_DWH to Database_Setup.sql

--- scripts/Database_Setup.sql
+++ scripts/Database_Setup.sql
@@ -1,9 +1,13 @@
--- 1. create OLTP database
+-- 1. create DWH database
+DROP DATABASE IF EXISTS VisionAirport_DWH;
+CREATE DATABASE VisionAirport_DWH;
+
+-- 2. create OLTP database
 DROP DATABASE IF EXISTS VisionAirport_OLTP;
 CREATE DATABASE VisionAirport_OLTP;
 USE VisionAirport_OLTP;
 
--- 2. create schema's in db VisionAirport_OLTP
+-- 3. create schema's in db VisionAirport_OLTP
 CREATE SCHEMA [RAW];
 GO
 CREATE SCHEMA [ARCHIVE];
@@ -11,4 +15,4 @@ GO
 CREATE SCHEMA [CLEANSED];
 GO
 
--- 3. import flat data files in [VisionAirport_OLTP].[RAW] > Import Wizard
+-- 4. import flat data files in [VisionAirport_OLTP].[RAW] > Import Wizard
