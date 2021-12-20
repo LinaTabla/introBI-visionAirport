1ef46277871e7cb723873894dd6b18c02746d44a
LinaTabla <lina.tabla@hotmail.com>
2021-12-20 23:48:54 +0100

    Updated packages

---
+++ Scripts/CLEANSED/Cleanse_Tijd_Datum.sql
@@ -0,0 +1,76 @@
+USE [VisionAirport_OLTP];
+
+-- Generate our own data.
+DECLARE @StartDate  date = '20140101';
+DECLARE @CutoffDate date = DATEADD(DAY, -1, DATEADD(YEAR, 30, @StartDate));
+
+-- SET LANGUAGE Dutch;
+SET LANGUAGE us_english;
+
+WITH seq(n) AS 
+(
+  SELECT 0 UNION ALL SELECT n + 1 FROM seq
+  WHERE n < DATEDIFF(DAY, @StartDate, @CutoffDate)
+),
+d(d) AS 
+(
+  SELECT DATEADD(DAY, n, @StartDate) FROM seq
+),
+src AS
+(
+  SELECT
+    TheDate         = CONVERT(date, d),
+    TheDay          = DATEPART(DAY,       d),
+    TheDayName      = DATENAME(WEEKDAY,   d),
+    TheWeek         = DATEPART(WEEK,      d),
+    TheISOWeek      = DATEPART(ISO_WEEK,  d),
+    TheDayOfWeek    = DATEPART(WEEKDAY,   d),
+    TheMonth        = DATEPART(MONTH,     d),
+    TheMonthName    = DATENAME(MONTH,     d),
+    TheQuarter      = DATEPART(Quarter,   d),
+    TheYear         = DATEPART(YEAR,      d),
+    TheFirstOfMonth = DATEFROMPARTS(YEAR(d), MONTH(d), 1),
+    TheLastOfYear   = DATEFROMPARTS(YEAR(d), 12, 31),
+    TheDayOfYear    = DATEPART(DAYOFYEAR, d)
+  FROM d
+),
+dim AS
+(
+  SELECT
+	DagNummer		=	TheDay,
+	DagTekst		=	TheDayName,
+	FiscaleWeek		=	CASE WHEN (TheWeek - 1) <= 0 THEN TheWeek + 52 ELSE TheWeek - 1 END,
+	Maand			=	TheMonth,
+	Jaar			=	TheYear,
+	VolledigeDatum	=	TheDate
+  FROM src
+)
+INSERT
+	INTO [CLEANSED].[generated_datum]
+	SELECT * 
+		FROM dim
+		ORDER BY VolledigeDatum
+		OPTION (MAXRECURSION 0)
+GO
+
+DECLARE @hour	int = 0;
+DECLARE @minute int = 0;
+
+WHILE @hour < 24
+BEGIN
+	SET @minute = 0;
+
+	WHILE @minute < 60
+	BEGIN
+		INSERT INTO [CLEANSED].[generated_time]
+			SELECT
+			(@hour*100) + @minute as TimeKey,
+			@hour as [Hour],
+			@minute as [Minute];
+
+		SET @minute = @minute + 1;
+	END;
+
+	SET @hour = @hour + 1;
+END
+GO
--- Scripts/CLEANSED/Cleansed_Aankomst.sql
+++ Scripts/CLEANSED/Cleansed_Aankomst.sql
@@ -1,14 +1,18 @@
 USE [VisionAirport_OLTP];
 
-INSERT INTO [CLEANSED].[vertrek]
+INSERT INTO [CLEANSED].[aankomst] 
 	SELECT 
-		CAST(CAST(REPLACE(VluchtId, ',', '.') AS float) AS int),
-		CAST(VliegtuigCode AS varchar(10)), 
+		CAST(CAST(REPLACE(A.VluchtId, ',', '.') AS float) AS int),
+		CAST(A.VliegtuigCode AS varchar(10)), 
 		NULLIF(CAST(Terminal AS char(1)), ''), 
 		NULLIF(CAST(Gate AS char(2)), ''), 
 		NULLIF(CAST(Baan AS int), ''), 
 		NULLIF(CAST(Bezetting AS int), ''), 
-		NULLIF(CAST(Vracht AS int), ''), 
-		NULLIF(CAST(Vertrektijd AS datetime), '')			
-	FROM [RAW].[export_vertrek]
-	WHERE NULLIF(CAST(Vertrektijd AS datetime), '') IS NOT NULL;
+		NULLIF(CAST(Vracht AS int), ''),
+		NULLIF(CAST(AankomstTijd AS DATE), ''),
+		REPLACE(DATEPART(HOUR, AankomstTijd), '', 0),
+		REPLACE(DATEPART(MINUTE, AankomstTijd), '', 0)
+	FROM [RAW].[export_aankomst] A
+		LEFT OUTER JOIN [CLEANSED].[vlucht] V
+			ON V.VluchtId = CAST(CAST(REPLACE(A.VluchtId, ',', '.') AS float) AS int)
+	WHERE Aankomsttijd <> '' AND V.VluchtId IS NOT NULL;
